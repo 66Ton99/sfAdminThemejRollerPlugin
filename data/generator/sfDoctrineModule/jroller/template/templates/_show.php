e19d9a1d7a49336d4578b6ce965143eb69e81d6d
Ton Sharp <Forma-PRO@66ton99.org.ua>
2011-06-09 16:36:56 +0300

    Added data format for show action

--- data/generator/sfDoctrineModule/jroller/template/templates/_show.php
+++ data/generator/sfDoctrineModule/jroller/template/templates/_show.php
@@ -32,9 +32,16 @@
 		      [?php include_component('<?php echo $this->getModuleName() ?>', $name, array('<?php echo $this->getSingularName() ?>' => $<?php echo $this->getSingularName() ?>, 'form' => $form, 'attributes' => $attributes instanceof sfOutputEscaper ? $attributes->getRawValue() : $attributes)) ?]
 		    [?php else: ?]
 		    <div class="sf_admin_form_row">
-		      <label>[?php echo $field->getConfig('label')? $field->getConfig('label'): $field->getName() ?]:</label>
-			    [?php echo $form->getObject()->get($name) ? $form->getObject()->get($name) : "&nbsp;" ?]
-		    </div>
+          <label>[?php echo $field->getConfig('label')? $field->getConfig('label'): $field->getName() ?]:</label>
+          [?php if(sfConfig::get('app_sf_admin_theme_jroller_plugin_show_data_format') &&
+                   !is_numeric($form->getObject()->get($name)) &&
+                   strtotime($form->getObject()->get($name))):?]
+            [?php extract(sfConfig::get('app_sf_admin_theme_jroller_plugin_show_data_format')) ?]
+            [?php echo format_date($form->getObject()->get($name), @$format, @$culture, @$charset) ?]
+          [?php else: ?]
+            [?php echo $form->getObject()->get($name) ? $form->getObject()->get($name) : "&nbsp;" ?]
+          [?php endif; ?]
+        </div>
 		    [?php endif; ?]
 
 		  [?php endforeach; ?]
